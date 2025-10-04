03fdced25c3678d164fbe1697cce27d1dd75fadb
Dru Still <eugene_still@brown.edu>
2025-10-04 17:34:23 -0400

    Basic SDL window

--- main.c
+++ main.c
@@ -1,5 +1,46 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <stdio.h>
 
-int main() {
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+int main()
+{
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  if (!SDL_CreateWindowAndRenderer("Flurry", 640, 480, 0, &window, &renderer)) {
+    SDL_Log("Couldn't create video/renderer: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  int running = 1;
+  while (running) {
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = 0;
+      }
+    }
+
+    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
+    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
+    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
+    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+
+    /* clear the window to the draw color. */
+    SDL_RenderClear(renderer);
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(renderer);
+
+  }
+
   return 0;
 }
