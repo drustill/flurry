71139e143e9c802a5e2263e4fec2259b3f4b6373
Dru Still <eugene_still@brown.edu>
2025-10-14 07:50:04 -0400

    Overhaul

--- main.c
+++ main.c
@@ -21,14 +21,14 @@ static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
 typedef struct {
-  float x, y;
+  float x, y, z;
   float phase;
   float orbit_speed;
   float orbit_radius;
 } Magnet;
 
 typedef struct {
-  float x, y, vx, vy;
+  float x, y, z, vx, vy, vz;
   double birthday;
 } Particle;
 
@@ -57,6 +57,7 @@ int main()
     magnets[i].orbit_speed = 0.3f + (float)(NUM_MAGNETS - i) * 0.3f;
     magnets[i].orbit_radius = 80.0f + (float)(NUM_MAGNETS - i) * 20.0f;
     magnets[i].phase = (float)i * 2.0f * M_PI / NUM_MAGNETS;
+    magnets[i].z = 2000.0f;
   }
 
   uint64_t curr, prev;
@@ -77,17 +78,20 @@ int main()
     const double now = ((double)curr) / 1000.0;
 
     for (int i = 0; i < NUM_MAGNETS; i++) {
-      float angle = now * magnets[i].orbit_speed + magnets[i].orbit_radius;
-      magnets[i].x = center_x + cosf(angle) * magnets[i].orbit_radius;
-      magnets[i].y = center_y + sinf(angle) * magnets[i].orbit_radius;
+      float angle = now * magnets[i].orbit_speed + magnets[i].phase;
+      magnets[i].x = cosf(angle) * magnets[i].orbit_radius;
+      magnets[i].y = sinf(angle) * magnets[i].orbit_radius;
+      magnets[i].z = sinf(angle * 0.5f) * 50.0f + 2000.0f;
     }
 
     for (int k = 0; k < 3; k++) {
       particles[pidx].birthday = now;
-      particles[pidx].x = center_x + RANDF() * 10.0f;
-      particles[pidx].y = center_y + RANDF() * 10.0f;
+      particles[pidx].x = RANDF() * 10.0f;
+      particles[pidx].y = RANDF() * 10.0f;
+      particles[pidx].z = 2000.0f + RANDF() * 100.0f;
       particles[pidx].vx = RANDF() * 20.0f;
       particles[pidx].vy = RANDF() * 20.0f;
+      particles[pidx].vz = RANDF() * 20.0f;
       pidx = (pidx + 1) % MAX_PARTICLES;
     }
 
@@ -95,7 +99,8 @@ int main()
       for (int j = 0; j < NUM_MAGNETS; j++) {
         float dx = particles[i].x - magnets[j].x;
         float dy = particles[i].y - magnets[j].y;
-        float rsquared = dx*dx + dy*dy;
+        float dz = particles[i].z - magnets[j].z;
+        float rsquared = dx*dx + dy*dy + dz*dz;
 
         if (rsquared < 2500.0f) {
           rsquared = 2500.0f;
@@ -106,13 +111,16 @@ int main()
 
         particles[i].vx -= (dx * magnitude) * elapsed;
         particles[i].vy -= (dy * magnitude) * elapsed;
+        particles[i].vz -= (dz * magnitude) * elapsed;
       }
 
       particles[i].vx *= DRAG;
       particles[i].vy *= DRAG;
+      particles[i].vz *= DRAG;
 
       particles[i].x += particles[i].vx * elapsed;
       particles[i].y += particles[i].vy * elapsed;
+      particles[i].z += particles[i].vz * elapsed;
     }
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -120,7 +128,9 @@ int main()
 
     for (int i = 0; i < NUM_MAGNETS; i++) {
       SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-      SDL_FRect magnet_rect = { magnets[i].x - 5.0f, magnets[i].y - 5.0f, 10.0f, 10.0f };
+      float screen_x = (magnets[i].x * WIDTH / magnets[i].z) + WIDTH * 0.5f;
+      float screen_y = (magnets[i].y * HEIGHT / magnets[i].z) + HEIGHT * 0.5f;
+      SDL_FRect magnet_rect = { screen_x - 5.0f, screen_y - 5.0f, 10.0f, 10.0f };
       SDL_RenderFillRect(renderer, &magnet_rect);
     }
 
@@ -136,12 +146,18 @@ int main()
       float alpha = (1.0f - (age / LIFETIME)) * 0.5f;
 
       if (alpha > 0) {
+        float screen_x = (particles[j].x * WIDTH / particles[j].z) + WIDTH * 0.5f;
+        float screen_y = (particles[j].y * HEIGHT / particles[j].z) + HEIGHT * 0.5f;
+        float size = 500.0f / particles[j].z;
+        if (size < 1.0f) {
+          size = 1.0f;
+        }
         SDL_SetRenderDrawColorFloat(renderer, r, g, b, alpha);
         SDL_FRect rect = {
-          particles[j].x - 3.0f,
-          particles[j].y - 3.0f,
-          6.0f,
-          6.0f
+          screen_x - size,
+          screen_y - size,
+          size * 2.0f,
+          size * 2.0f
         };
         SDL_RenderFillRect(renderer, &rect);
       }
